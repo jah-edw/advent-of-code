6d58827771be6debdb8136969c02832b9b633b6e
jah-edw <jah.edwards@hotmail.com>
2021-12-02 12:46:56 +0000

    task1 solution implemented

---
+++ day2/task1.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+function dataToArr(filePathString) {
+    let contentString = fs.readFileSync(filePathString).toString();
+    let contentArr = contentString.split(/\n| /)
+    return contentArr
+}
+
+function depthPerception(fileNameString) {
+    let arr = dataToArr(fileNameString)
+    let forward = [];
+    let up = [];
+    let down = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'forward') forward.push(arr[i + 1])
+        if (arr[i] === 'up') up.push(arr[i + 1])
+        if (arr[i] === 'down') down.push(arr[i + 1])
+    }
+
+    let finalUp = up.map(Number).reduce((prev, current) => prev + current)
+    let finalDown = down.map(Number).reduce((prev, current) => prev + current)
+    let finalDistance = forward.map(Number).reduce((prev, current) => prev + current)
+
+    return (finalDown - finalUp) * finalDistance;
+}
+
+console.log(depthPerception("data.txt"))
